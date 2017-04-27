71fb1e9befe368e1288382d44d20246dce724a56
aabbatiello <anthonyjabbatiello@gmail.com>
2017-04-27 13:46:29 -0400

    My_Homework

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,97 +1,51 @@
-var panel = $('#quiz-area');
-var countStartNumber = 30;
 
 
-$(document).on('click', '#start-over', function() {
-  game.reset();
-});
-
-$(document).on('click', '#start', function() {
-  $('#subwrapper').prepend('<h2>Time Remaining: <span id="counter-number">30</span> Seconds</h2>');
-  game.loadQuestion();
-});
+function onSubmit() {
+  var score=0;
+  var questions=5;
+  var answers= ["a","b","a","c","c"];
 
 
-
-var questions = [{
-  question: "What kind of jeans do you prefer?",
-  answers: ["Skinny Jeans", "Baggy Jeans", "Boot Cut Jeans"],
-  correctAnswer: "skinny jeans",
-
-}, {
-  question: "What kind of cigarettes do you smoke?",
-  answers: ["Marlboro Lights", "American Spirits", "Newports"],
-  correctAnswer: "American Spriits",
-}, {
-  question: "How do you prefer to get around?",
-  answers: ["Prius", "Hummer", "Lamborghini"],
-  correctAnswer: "Prius",
- 
-}, {
-  question: "Which of these beers would you bring to a party?",
-  answers: ["Budwiser", "Corona", "Brooklyn"],
-  correctAnswer: "Brooklyn",
-}, {
-  question: "Which of these authors would you prefer reading?",
-  answers: ["Ernest Hemingway", "Geroge Orwall", "Jack Keraouac"],
-  correctAnswer: "Jack Keraouac",
- 
-}];
+var q1 = document.forms["quiz"]["q1"].value;
+var q2 = document.forms["quiz"]["q2"].value;
+var q3 = document.forms["quiz"]["q3"].value;
+var q4 = document.forms["quiz"]["q4"].value;
+var q5 = document.forms["quiz"]["q5"].value;
 
 
-var game = {
-  questions:questions,
-  counter:countStartNumber,
-  correct:0,
-  incorrect:0,  
- 
- countdown: function(){
-    game.counter--;
-    $('#counter-number').html(game.counter);
+for(var i =1; i <= questions; i++){
+  if(eval("q" + i) === ""){
+    alert("You missed question number " + i);
+}
+}
 
-    if (game.counter === 0){
-      game.timeUp();
-    }
-  },
+for(var i =1; i <= questions; i++){
+  if(eval("q" + i) === answers[i - 1]){
+    score++;
+ }
+}
+
+var results = document.getElementById("results");
+results.innerhtml="<h2> You scored " + results + " points out of " + questions+ "</h2>"
+alert("You scored  "+ score  + " out of " + questions);
+return false;
+restart();
+}
 
 
-  loadQuestion: function(){
-    timer = setInterval(game.countdown, 1000);
-    for (var i = 0; i <questions[this.question]; i++){
-      panel.append('<button class="answer-button" id="button"' + 'data-name="' + questions[this.currentQuestion].answers[i] + '">' + questions[this.currentQuestion].answers[i]+ '</button>');
-    }
-  },
- 
-  timeUp: function (){
-    clearInterval(timer);
-    $('#counter-number').html(game.counter);
+var count=15;
 
-    panel.html('<h2>Out of Time!</h2>');
-    
+var counter=setInterval(timer, 1000); 
 
-    if (game.question === questions.length - 1){
-      setTimeout(game.results);
-    } else {
-      setTimeout(game.results);
-    }
-  },
-  results: function() {
-    clearInterval(timer);
+function timer()
+{
+  count=count-1;
+  if (count <= 0)
+  {
+     clearInterval(counter);
+     alert("Times up compadre, Game Over!");
 
-    panel.html('<h2>All done, heres how you did!</h2>');
-    $('#counter-number').html(game.counter);
-    panel.append('<h3>Correct Answers: ' + game.correct + '</h3>');
-    panel.append('<h3>Incorrect Answers: ' + game.incorrect + '</h3>');
-    panel.append('<h3>Unanswered: ' + (questions.length - (game.incorrect + game.correct)) + '</h3>');
-    panel.append('<br><button id="start-over">Start Over?</button>');
-  },
+   }
 
-  
-  reset: function(){
-    this.question = 0;
-    this.counter = countStartNumber;
-    this.correct = 0;
-    this.incorrect = 0;
-    this.loadQuestion();
-  }
-};
+ document.getElementById("timer").innerHTML=count + " secs";
+}
